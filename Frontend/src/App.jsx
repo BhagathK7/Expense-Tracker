de4c28e47a8a4ee0976f63043a71b1f7da71ff5b
BhagathK7 <bhagathkrishnasunilkumar.23cse@kongu.edu>
2025-06-26 15:10:25 +0530

    First Commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+
+import ExpenseContainer from "./components/ExpenseContainer";
+
+const App=() => {
+  return (
+    <div className="App">
+      <ExpenseContainer/>
+    </div>
+  );
+}
+
+export default App;
